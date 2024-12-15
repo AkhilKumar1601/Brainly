dabce5052142772f99e27c99423476186e9cfeda
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2024-12-15 15:22:49 +0530

    Frontend added

--- Brainly-frontend/src/App.tsx
+++ Brainly-frontend/src/App.tsx
@@ -1,18 +1,15 @@
-import { Button } from './components/Button'
-import { Card } from './components/Card'
-import { AddIcon } from './icon/AddIcon'
-import { ShareIcon } from './icon/ShareIcon'
-
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Dashboard } from "./pages/dashboard"
 function App() {
-
-  return (
-    <>
-     <Button variant="primary" text='Add Content' icon={<AddIcon/>}></Button>
-     <Button variant='secondary' text = 'Share Brain' icon={<ShareIcon/>}></Button>
-
-     <Card />
-    </>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
--- Brainly-frontend/src/components/Button.tsx
+++ Brainly-frontend/src/components/Button.tsx
@@ -1,23 +1,27 @@
 import { ReactElement } from "react";
 
-interface ButtonProp {
-  variant : "primary" | "secondary";
-  text : string;
-  icon : ReactElement
+interface ButtonProps {
+    variant: "primary" | "secondary";
+    text: string;
+    startIcon: ReactElement;
+    onClick?: () => void;
+    fullWidth?: boolean;
+    loading?: boolean;
 }
 
 const variantClasses = {
-  "primary" : "bg-purple-600 text-white",
-  "secondary" : "bg-purple-200 text-purple-600",
-}
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-200 text-purple-600",
+};
 
-const defaultStyles = "px-4 py-2 rounded-md font-light flex items-center"
+const defaultStyles = "px-4 py-2 rounded-md font-light flex items-center";
 
-export const Button = ({variant, text, icon} : ButtonProp) => {
-  return <button className={variantClasses[variant] + " " + defaultStyles}>
-    <div className="pr-2">
-     {icon}
-    </div>
-    {text}
-  </button>
+
+export function Button({variant, text, startIcon, onClick, fullWidth, loading}: ButtonProps) {
+    return <button onClick={onClick} className={variantClasses[variant] + " " + defaultStyles + `${fullWidth ? " w-full flex justify-center items-center" : ""} ${loading ? "opacity-45	" : ""}`} disabled={loading}>
+        <div className="pr-2">
+            {startIcon}
+        </div>
+        {text}
+    </button>
 }
